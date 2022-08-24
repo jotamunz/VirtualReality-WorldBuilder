5f1cba4bfa7dcb8edf94676d7442e34a6b687d99
Jose Pablo Muñoz <jpmunoz@estudiantec.cr>
2022-08-24 15:10:26 -0600

    Added Interaction to items

--- Assets/Scripts/objectCreator.cs
+++ Assets/Scripts/objectCreator.cs
@@ -1,13 +1,11 @@
-    using System;
+using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class objectCreator : MonoBehaviour
 {
-    private const float DEFAULT_SCALE = 0.5f;
-    private float scaleX = DEFAULT_SCALE, scaleY = DEFAULT_SCALE, scaleZ = DEFAULT_SCALE;    
-    public string objectTypeName;
-    private AudioSource source;
+    public float DEFAULT_SCALE;  
+    public GameObject newObject;
 
    void Start()
     {
@@ -19,27 +17,8 @@ public class objectCreator : MonoBehaviour
 
 
     public void createObject() 
-    {
-        GameObject newObject;   
-        switch(objectTypeName){
-            case "sphere":
-                 newObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                break;
-            case "cube":
-                newObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                break;
-            case "cylinder":
-                newObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                break;
-            default: 
-                newObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                break;
-        }
-        
-        newObject.transform.position = new Vector3(0, 3, 1);
-        newObject.transform.localScale = new Vector3(scaleX,scaleY,scaleZ);
-        newObject.AddComponent<Rigidbody>();
-        newObject.AddComponent<XRGrabInteractable>();
-        
+    {   
+        newObject.transform.localScale = new Vector3(DEFAULT_SCALE, DEFAULT_SCALE, DEFAULT_SCALE);
+        Instantiate(newObject, new Vector3(0, 1, 1), Quaternion.identity);
     }
 }
