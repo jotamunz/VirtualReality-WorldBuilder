a2f2abb88c3cf586b40ce43d41cee85f280abb59
joseSalazar4 <mikomeka1@gmail.com>
2022-08-29 23:02:08 -0600

    Added Locomotion & Fixed scale issue

---
+++ test_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CharacterMovementHelper::Start()
+extern void CharacterMovementHelper_Start_m18955692205974A38B1A358AC936D5C0B68A8599 (void);
+// 0x00000002 System.Void CharacterMovementHelper::Update()
+extern void CharacterMovementHelper_Update_m897D1E408300AD2AF8DA85D7F897D5F0670AA2F1 (void);
+// 0x00000003 System.Void CharacterMovementHelper::UpdateCharacterController()
+extern void CharacterMovementHelper_UpdateCharacterController_m2755EA4C5558605C27EA1E28C312BD8D1FB01001 (void);
+// 0x00000004 System.Void CharacterMovementHelper::.ctor()
+extern void CharacterMovementHelper__ctor_m307CAC667F6618A0975782724FDD11DC9A4C12FE (void);
+// 0x00000005 System.Void CreateRuntimeObject::createNewObject()
+extern void CreateRuntimeObject_createNewObject_m2A6EC358ACE3AE1EABD8B360629A08F31A889902 (void);
+// 0x00000006 System.Void CreateRuntimeObject::.ctor()
+extern void CreateRuntimeObject__ctor_m460C252CCD4C3CEA23288F4914CCAF3F276B792E (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	CharacterMovementHelper_Start_m18955692205974A38B1A358AC936D5C0B68A8599,
+	CharacterMovementHelper_Update_m897D1E408300AD2AF8DA85D7F897D5F0670AA2F1,
+	CharacterMovementHelper_UpdateCharacterController_m2755EA4C5558605C27EA1E28C312BD8D1FB01001,
+	CharacterMovementHelper__ctor_m307CAC667F6618A0975782724FDD11DC9A4C12FE,
+	CreateRuntimeObject_createNewObject_m2A6EC358ACE3AE1EABD8B360629A08F31A889902,
+	CreateRuntimeObject__ctor_m460C252CCD4C3CEA23288F4914CCAF3F276B792E,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	2579,
+	2579,
+	2579,
+	2579,
+	2579,
+	2579,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
