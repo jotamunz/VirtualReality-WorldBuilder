0038422d047bba1ee041c387527bdb65973808fc
Jose Pablo Muñoz <jpmunoz@estudiantec.cr>
2022-09-27 23:15:17 -0600

    Added crop drops

--- Assets/Scripts/DropCrop.cs
+++ Assets/Scripts/DropCrop.cs
@@ -7,7 +7,7 @@ public class DropCrop : MonoBehaviour
 {
     public GameObject seedDropPrefab, cropDropPrefab;
     public int seedDropAmount, cropDropAmount, hitpoints;
-    private float spawnDelay = 0.4f;
+    private float spawnDelay = 0.2f;
 
     void Start()
     {
@@ -24,32 +24,44 @@ public class DropCrop : MonoBehaviour
         if (collision.gameObject.tag == "Scythe") hitpoints--;
         if (collision.gameObject.tag == "Scythe" && hitpoints == 0)
         {
-            GenerateDrops(seedDropPrefab, seedDropAmount);
-            GenerateDrops(cropDropPrefab, cropDropAmount);
-            Destroy(gameObject, spawnDelay);
-        }
-        yield return new WaitForSeconds(0f);
-    }
+            // TODO: Cant refactor this for some reason
+            float[] angles = {10, 30, 60, 90};  // This just could be a random within a range
+            LinkedList<float> anglesList = new LinkedList<float>(angles);
+            LinkedListNode<float> current = anglesList.First;
 
-    IEnumerator GenerateDrops(GameObject dropPrefab, int amount)
-    {
-        float[] angles = { 10, 30, 60, 90};  // This just could be a random within a range
-        LinkedList<float> anglesList = new LinkedList<float>(angles);
-        LinkedListNode<float> current = anglesList.First;
+           // Spawn the crops
+            for(int counter=0; counter < cropDropAmount; counter++){
+                yield return new WaitForSeconds(spawnDelay);
+                GameObject drop = Instantiate(cropDropPrefab, transform.position, transform.rotation);
 
-        for(int counter=0; counter < amount; counter++){
-            yield return new WaitForSeconds(spawnDelay);
-            GameObject drop = Instantiate(dropPrefab, transform.position, transform.rotation);
+                // Position at a proper upper point
+                drop.transform.Translate(0.0f, 1.0f, 0.0f);
+                drop.transform.Rotate(0.0f, current.Value, 0.0f, Space.Self);
 
-            // Position at a proper upper point
-            drop.transform.Translate(0.0f, 1.0f, 0.0f);
-            drop.transform.Rotate(0.0f, current.Value, 0.0f, Space.Self);
+                // Apply force to the object
+                drop.GetComponent<Rigidbody>().AddForce(transform.up * 200);
+                drop.GetComponent<Rigidbody>().AddForce(transform.right * 50);                
 
-            // Apply force to the object
-            drop.GetComponent<Rigidbody>().AddForce(transform.up * 200);
-            drop.GetComponent<Rigidbody>().AddForce(transform.right * 50);                
+                current = current.Next ?? current.List.First;
+            }
 
-            current = current.Next ?? current.List.First;
+            // Spawn the seeds
+            for(int counter=0; counter < seedDropAmount; counter++){
+                yield return new WaitForSeconds(spawnDelay);
+                GameObject drop = Instantiate(seedDropPrefab, transform.position, transform.rotation);
+
+                // Position at a proper upper point
+                drop.transform.Translate(0.0f, 1.0f, 0.0f);
+                drop.transform.Rotate(0.0f, current.Value, 0.0f, Space.Self);
+
+                // Apply force to the object
+                drop.GetComponent<Rigidbody>().AddForce(transform.up * 200);
+                drop.GetComponent<Rigidbody>().AddForce(transform.right * 50);                
+
+                current = current.Next ?? current.List.First;
+            }
+
+            Destroy(gameObject);
         }
     }
 }   
